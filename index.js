d5eb67c5f542efc806826cd9f9e554653bebb7c9
Sklearosi <117984274+Sklearosi@users.noreply.github.com>
2023-07-24 20:00:27 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,98 +1,8 @@
 import data from "./data.json" assert {type: "json"}
 let wrapper = document.getElementById("content")
 let currentUser = data.currentUser.username
+wrapper.classList.add('wrapper')
 
-function comments() {
-    
-    
-
-for(let i = 0; i < data.comments.length; i++){
-
-    let li = document.createElement('li')
-    li.classList.add('container')
-    li.innerHTML = `
-    <div id="info">
-    <img src="${data.comments[i].user.image.png}" >
-    <p id="username">${data.comments[i].user.username}</p>
-    <p id="createdAt">${data.comments[i].createdAt}</p>
-    </div>
-
-    <div id="comment"><P>${data.comments[i].content}</P></div>
-    <div id="last">
-    <div id="score"><img src="./images/icon-plus.svg"><p>${data.comments[i].score}</p><img src = "./images/icon-minus.svg"></div>
-    <div class="reply"><img src="./images/icon-reply.svg"><p>Reply</p></div>
-    </div>
-
-    `
-    wrapper.append(li)
-    
-    
-
-    if(data.comments[i].replies.length > 0){
-
-        const replyUl = document.createElement('ul')
-        replyUl.classList.add('replyUl')
-        
-
-        for(let j = 0; j < data.comments[i].replies.length; j++){
-
-            
-           
-
-            const replyLi = document.createElement('li')
-            replyLi.classList.add("replyLi")
-
-            replyLi.innerHTML = `
-
-        
-    ${data.comments[i].replies[j].user.username === currentUser?`<div id="replyInfoUser">
-    <img src="${data.comments[i].replies[j].user.image.png}" >
-    <p id="username">${data.comments[i].replies[j].user.username}</p><p id="you">you</p><p id="createdAt">${data.comments[i].replies[j].createdAt}</p>
-    </div>`:`<div id="replyInfo">
-    <img src="${data.comments[i].replies[j].user.image.png}" >
-    <p id="username">${data.comments[i].replies[j].user.username}</p><p id="createdAt">${data.comments[i].replies[j].createdAt}</p>
-    </div>`
-                
-    }
-    
-
-    <div id="comment"><P><span id="replying">@${data.comments[i].replies[j].replyingTo}</span> ${data.comments[i].replies[j].content}</P></div>
-    <div id="last">
-    <div id="score"><img src="./images/icon-plus.svg"><p>${data.comments[i].replies[j].score}</p><img src = "./images/icon-minus.svg"></div>
-    
-    ${data.comments[i].replies[j].user.username === currentUser?`<div id="buttons"><button id="deleteComment"><img src="./images/icon-delete.svg"> Delete</button><button><img src="./images/icon-edit.svg">Edit</button></div>`:`<div class="reply"><img src="./images/icon-reply.svg"><p>Reply</p></div>`
-                
-    }
-    </div>
-
-    
-            
-
-            `
-
-           
-
-            replyUl.append(replyLi)
-            wrapper.append(replyUl)
-            
-
-
-            
-
-
-        }
-
-
-    
-    }
-
-
-}
-
-
-
-
-}
 
 
 function commentAndDelete() {
@@ -214,31 +124,292 @@ newCommentSection.addEventListener("click", (eventTwo) => {
 }
 
     
-wrapper.addEventListener('click', (e) => {
-    if(e.target.parentElement.classList.contains('reply')) {
-        const som = {
-            "id": 5,
-            "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-            "createdAt": "2 days ago",
-            "score": 2,
-            "replyingTo": "ramsesmiron",
-            "user": {
-              "image": { 
-                "png": "./images/avatars/image-juliusomo.png",
-                "webp": "./images/avatars/image-juliusomo.webp"
-              },
-              "username": "juliusomo"
+
+
+
+function renderComments(commentsArray) {
+    wrapper.innerHTML = ''; 
+  
+    for(let i = 0; i < commentsArray.length; i++){
+
+        let li = document.createElement('li')
+        li.classList.add('container')
+        li.innerHTML = `
+        <div id="info">
+        <img src="${data.comments[i].user.image.png}" >
+        <p id="username">${data.comments[i].user.username}</p>
+        <p id="createdAt">${data.comments[i].createdAt}</p>
+        </div>
+    
+        <div id="comment"><P>${data.comments[i].content}</P></div>
+        <div id="last">
+        <div id="score"><img src="./images/icon-plus.svg" class="plus"><p>${data.comments[i].score}</p><img src = "./images/icon-minus.svg" class="minus"></div>
+        ${data.comments[i].user.username === "maxblagun"?`<div class="replyMax"><img src="./images/icon-reply.svg"><p>Reply</p></div>`:`<div class="reply"><img src="./images/icon-reply.svg"><p>Reply</p></div>`
+                    
+        }
+        
+        </div>
+    
+        `
+        wrapper.append(li)
+        
+        
+    
+        if(commentsArray[i].replies.length > 0){
+    
+            const replyUl = document.createElement('ul')
+            replyUl.classList.add('replyUl')
+            
+    
+            for(let j = 0; j < commentsArray[i].replies.length; j++){
+    
+                
+               
+    
+                const replyLi = document.createElement('li')
+                replyLi.classList.add("replyLi")
+    
+                replyLi.innerHTML = `
+    
+            
+        ${data.comments[i].replies[j].user.username === currentUser?`<div id="replyInfoUser">
+        <img src="${data.comments[i].replies[j].user.image.png}" >
+        <p id="username">${data.comments[i].replies[j].user.username}</p><p id="you">you</p><p id="createdAt">${data.comments[i].replies[j].createdAt}</p>
+        </div>`:`<div id="replyInfo">
+        <img src="${data.comments[i].replies[j].user.image.png}" >
+        <p id="username">${data.comments[i].replies[j].user.username}</p><p id="createdAt">${data.comments[i].replies[j].createdAt}</p>
+        </div>`
+                    
+        }
+        
+    
+        <div id="comment"><P id="commented"><span id="replying">@${data.comments[i].replies[j].replyingTo}</span> ${data.comments[i].replies[j].content}</P></div>
+        <div id="last">
+        <div id="score"><img src="./images/icon-plus.svg"><p>${data.comments[i].replies[j].score}</p><img src = "./images/icon-minus.svg"></div>
+        
+        ${data.comments[i].replies[j].user.username === currentUser?`<div id="buttons"><button class="deleteComment"><img src="./images/icon-delete.svg"> Delete</button><button class="editButtonReply"><img src="./images/icon-edit.svg">Edit</button></div>`:`<div class="replyRamses"><img src="./images/icon-reply.svg"><p>Reply</p></div>`
+                    
+        }
+        </div>
+    
+        
+                
+    
+                `
+    
+               
+    
+                replyUl.append(replyLi)
+                wrapper.append(replyUl)
+                
+                
+    
+                
+    
+    
             }
-          }
-        data.comments[0].replies.push(som)
-        console.log(data.comments[0].replies);
-        return;
+    
+    
+        
+        }
+    
+    
     }
     
+  }
+
+  
+
+  function comments() {
+    
+    renderComments(data.comments);
+
+}
+
+
+
+wrapper.addEventListener('click', (e) => {
+    if(e.target.parentElement.classList.contains('reply')) {
+        let replyComment = document.createElement('div')
+        replyComment.classList.add('replyComment')
+        replyComment.innerHTML = `
+        <textarea id="replyTextArea" placeholder="@${e.target.parentElement.parentElement.parentElement.querySelector('#username').textContent}"></textarea>
+        <img src="${data.currentUser.image.png}">
+        <button id="replyCommentSection">REPLY</button>
+        `
+        let som = e.target.parentElement.parentElement.parentElement
+        wrapper.insertBefore(replyComment, som.nextSibling)
+        
+        const replyCommentFor = document.getElementById("replyCommentSection")
+        e.target.parentElement.style.pointerEvents = "none"
+replyCommentFor.addEventListener('click', (event) => {
+
+   const username = event.target.parentElement.previousSibling.querySelector('#username').textContent;
+    const replyTextArea = document.getElementById('replyTextArea').value
+    let replyArray = {
+        "id": 4,
+        "content":  replyTextArea ,
+        "createdAt": "2 days ago",
+        "score": 2,
+        "replyingTo": username,
+        "user": {
+          "image": { 
+            "png": "./images/avatars/image-juliusomo.png",
+            "webp": "./images/avatars/image-juliusomo.webp"
+          },
+          "username": "juliusomo"
+        }
+      }
+      
+      data.comments[0].replies.push(replyArray)
+      e.target.parentElement.style.pointerEvents = "unset"
+      renderComments(data.comments);
+})
+    } else if(e.target.parentElement.classList.contains('replyMax')) {
+        let replyComment = document.createElement('div')
+        replyComment.classList.add('replyComment')
+        replyComment.innerHTML = `
+        <textarea id="replyTextArea" placeholder="@${e.target.parentElement.parentElement.parentElement.querySelector('#username').textContent}"></textarea>
+        <img src="${data.currentUser.image.png}">
+        <button id="replyCommentSection">REPLY</button>
+        `
+        let som = e.target.parentElement.parentElement.parentElement
+        wrapper.insertBefore(replyComment, som.nextSibling)
+        
+        const replyCommentFor = document.getElementById("replyCommentSection")
+        e.target.parentElement.style.pointerEvents = "none"
+replyCommentFor.addEventListener('click', (event) => {
+   const username = event.target.parentElement.previousSibling.querySelector('#username').textContent;
+    const replyTextArea = document.getElementById('replyTextArea').value
+    let replyArray = {
+        "id": 4,
+        "content":  replyTextArea ,
+        "createdAt": "2 days ago",
+        "score": 2,
+        "replyingTo": username,
+        "user": {
+          "image": { 
+            "png": "./images/avatars/image-juliusomo.png",
+            "webp": "./images/avatars/image-juliusomo.webp"
+          },
+          "username": "juliusomo"
+        }
+      }
+      e.target.parentElement.style.pointerEvents = "usnet"
+      data.comments[1].replies.push(replyArray)
+      renderComments(data.comments);
+})
+    }else if(e.target.parentElement.classList.contains('replyRamses')) {
+        let replyComment = document.createElement('div')
+        replyComment.classList.add('replyCommentrams')
+        replyComment.innerHTML = `
+        <textarea id="replyTextArea" placeholder="@${e.target.parentElement.parentElement.parentElement.querySelector('#username').textContent}"></textarea>
+        <img src="${data.currentUser.image.png}">
+        <button id="replyCommentSection">REPLY</button>
+        `
+        let som = e.target.parentElement.parentElement.parentElement
+        let container = e.target.parentElement.parentElement.parentElement.parentElement
+        container.insertBefore(replyComment, som.nextSibling)
+        
+        const replyCommentFor = document.getElementById("replyCommentSection")
+        e.target.parentElement.style.pointerEvents = "none"
+replyCommentFor.addEventListener('click', (event) => {
+   const username = event.target.parentElement.previousSibling.querySelector('#username').textContent;
+    const replyTextArea = document.getElementById('replyTextArea').value
+    let replyArray = {
+        "id": 4,
+        "content":  replyTextArea ,
+        "createdAt": "2 days ago",
+        "score": 2,
+        "replyingTo": username,
+        "user": {
+          "image": { 
+            "png": "./images/avatars/image-juliusomo.png",
+            "webp": "./images/avatars/image-juliusomo.webp"
+          },
+          "username": "juliusomo"
+        }
+      }
+      e.target.parentElement.style.pointerEvents = "usnet"
+      data.comments[1].replies.push(replyArray)
+      renderComments(data.comments);
+})
+    } else if(e.target.classList.contains('deleteComment')) {
+        let popUp = document.createElement('div')
+        popUp.classList.add('popUp')
+        popUp.innerHTML = `
+        <p>Delete comment</p>
+        <p>Are you sure you want to delete this comment? This will remove the comment and can’t be undone.</p>
+        <div>
+        <button class="yes">Yes, DELETE</button>
+        <button class="no">No, CANCEL</button>
+        </div>
+        `
+        
+        document.querySelector('.writeComment').style.filter = "blur(8px)"
+        document.querySelector('.writeComment').style.pointerEvents = "none"
+        wrapper.style.filter = "blur(8px)"
+        wrapper.style.pointerEvents = "none"
+        document.body.style.overflow = "hidden"
+        document.body.append(popUp)
+        popUp.style.top = window.pageYOffset + 300 + "px"
+        
+        popUp.addEventListener("click", (a) => {
+            if(a.target.classList.contains("yes")){
+                e.target.parentElement.parentElement.parentElement.remove()
+                a.target.parentElement.parentElement.remove()
+                document.querySelector('.writeComment').style.filter = "unset"
+        document.querySelector('.writeComment').style.pointerEvents = "none"
+        wrapper.style.filter = "unset"
+        wrapper.style.pointerEvents = "unset"
+        document.body.style.overflow = "unset"
+            } else if(a.target.classList.contains("no")) {
+                a.target.parentElement.parentElement.remove()
+                document.querySelector('.writeComment').style.filter = "unset"
+        document.querySelector('.writeComment').style.pointerEvents = "none"
+        wrapper.style.filter = "unset"
+        wrapper.style.pointerEvents = "unset"
+        document.body.style.overflow = "unset"
+            }
+        })
+        
+    } else if(e.target.classList.contains('editButtonReply')){
+      const oldText = e.target.parentElement.parentElement.parentElement.querySelector('#commented')
+      const newText = document.createElement('textarea')
+        newText.classList.add('newText')
+        newText.textContent = oldText.textContent
+        oldText.replaceWith(newText)
+        const save = document.createElement('button')
+        save.classList.add('saveReply')
+        save.textContent = "UPDATE"
+        e.target.parentElement.parentElement.parentElement.querySelector('#comment').append(save)
+        save.addEventListener('click', () => {
+            oldText.textContent =  newText.value
+            newText.replaceWith(oldText)
+            save.remove()
+        })
+    }
 })
 
 
 
+wrapper.addEventListener('click', (a) => {
+    if (a.target.classList.contains('plus')) {
+        const pTag = a.target.parentElement.querySelector('#score p')
+        const currentNumber = parseInt(pTag.textContent, 10);
+        pTag.textContent = (currentNumber + 1).toString();
+        document.querySelector('.plus').style.pointerEvents = "none"
+        document.querySelector('.minus').style.pointerEvents = "unset"
+        document.querySelector('.minus').style.cursor = "pointer"
+    } else if (a.target.classList.contains('minus')) {
+        const pTag = a.target.parentElement.querySelector('#score p')
+        const currentNumber = parseInt(pTag.textContent, 10);
+        pTag.textContent = (currentNumber - 1).toString();
+        document.querySelector('.minus').style.pointerEvents = "none"
+        document.querySelector('.plus').style.pointerEvents = "unset"
+        document.querySelector('.plus').style.cursor = "pointer"
+    }
+})
 
 
 comments()
